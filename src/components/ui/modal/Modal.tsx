9eec43824a74899f44208b861485602f33c5741d
JeongHyeonKim <kjh985264@gmail.com>
2026-03-04 18:34:45 +0900

    feat: 공통 UI 컴포넌트 라이브러리 구현
    - Button, Input, DropDown, Modal, Toast 레이아웃 및 스타일 적용
    - 아이콘 컴포넌트화 및 index.ts 배럴 익스포트 적용

---
+++ src/components/ui/modal/Modal.tsx
@@ -0,0 +1,109 @@
+import gsap from 'gsap';
+import { useState, useEffect, useRef } from 'react';
+
+import { SignIn, SignUp } from '@/features/auth';
+import { ProfileSetting, Withdrawal } from '@/features/profile';
+import { useModalStore } from '@/store/modalStore';
+
+import styles from './Modal.module.scss';
+
+export default function Modal() {
+  const { isOpen, modalType, closeModal } = useModalStore();
+  const modalRef = useRef<HTMLDivElement>(null);
+  const backDropRef = useRef<HTMLDivElement>(null);
+  const closeModalRef = useRef(closeModal);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const container = useRef<HTMLDivElement>(null);
+  const startAnimation = useRef<gsap.core.Timeline | null>(null);
+
+  useEffect(() => {
+    closeModalRef.current = closeModal;
+  }, [closeModal]);
+
+  const handleCloseAnimation = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
+  };
+
+  useEffect(() => {
+    if (!isOpen && !isAnimating) return;
+    startAnimation.current?.kill();
+
+    startAnimation.current = gsap.timeline({
+      defaults: {
+        duration: 0.5,
+      },
+    });
+
+    if (isOpen && !isAnimating) {
+      startAnimation.current
+        .fromTo(
+          modalRef.current,
+          { y: 50, yPercent: -50, opacity: 0 },
+          { y: 0, opacity: 1, clearProps: 'transform' }
+        )
+        .fromTo(backDropRef.current, { opacity: 0 }, { opacity: 1 }, '<0.03');
+    } else if (isAnimating) {
+      startAnimation.current
+        .fromTo(
+          modalRef.current,
+          { y: 0, opacity: 1 },
+          {
+            y: 50,
+            yPercent: -50,
+            opacity: 0,
+            clearProps: 'transform',
+          }
+        )
+        .fromTo(
+          backDropRef.current,
+          { opacity: 1 },
+          {
+            opacity: 0,
+            onComplete: () => {
+              setIsAnimating(false);
+              closeModalRef.current();
+            },
+          },
+          '<0.03'
+        );
+    }
+
+    return () => {
+      startAnimation.current?.kill();
+    };
+  }, [isOpen, isAnimating]);
+
+  if (!isOpen && !isAnimating) return null;
+
+  const renderModalType = () => {
+    switch (modalType) {
+      case 'signIn':
+        return <SignIn />;
+      case 'signUp':
+        return <SignUp />;
+      case 'profileSetting':
+        return <ProfileSetting />;
+      case 'withdrawal':
+        return <Withdrawal />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div ref={container}>
+      <div
+        ref={backDropRef}
+        className={styles.backDrop}
+        onClick={handleCloseAnimation}
+      ></div>
+      <div
+        ref={modalRef}
+        className={`${styles.container} ${modalType ? styles[modalType] : ''}`}
+      >
+        {modalType && renderModalType()}
+      </div>
+    </div>
+  );
+}
