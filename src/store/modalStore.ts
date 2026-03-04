b9d50b20ada4d727e35221806b5c927af70ba9ab
JeongHyeonKim <kjh985264@gmail.com>
2026-03-04 18:32:27 +0900

    feat: Modal 및 Toast 전역 상태 관리 적용
    - App 컴포넌트에 ModalStore, ToastStore 연동
    - Zustand 기반 상태 관리 로직 구현

---
+++ src/store/modalStore.ts
@@ -0,0 +1,45 @@
+import { create } from 'zustand';
+
+export const MODAL_TYPE = {
+  SIGN_IN: 'signIn',
+  SIGN_UP: 'signUp',
+  PROFILE_SETTING: 'profileSetting',
+  WITHDRAWAL: 'withdrawal',
+} as const;
+
+type ModalDataMap = {
+  signIn: undefined;
+  signUp: undefined;
+  profileSetting: undefined;
+  withdrawal: undefined;
+};
+
+type ModalType = (typeof MODAL_TYPE)[keyof typeof MODAL_TYPE];
+
+type ModalStore = {
+  isOpen: boolean;
+  modalType: ModalType | null;
+  modalData: ModalDataMap[ModalType] | null;
+  openModal: <T extends ModalType>(type: T, data?: ModalDataMap[T]) => void;
+  closeModal: () => void;
+};
+
+export const useModalStore = create<ModalStore>((set) => ({
+  isOpen: false,
+  modalType: null,
+  modalData: null,
+
+  openModal: (type, data) =>
+    set({
+      modalType: type,
+      modalData: data ?? null,
+      isOpen: true,
+    }),
+
+  closeModal: () =>
+    set({
+      modalType: null,
+      modalData: null,
+      isOpen: false,
+    }),
+}));
