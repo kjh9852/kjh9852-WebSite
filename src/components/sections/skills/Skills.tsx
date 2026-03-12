24e1a9df305a9bd0248ec941b5dafe0f219b0a46
JeongHyeonKim <kjh985264@gmail.com>
2026-03-12 20:56:01 +0900

    feat: Skills 섹션 구현 및 렌딩페이지에 추가

---
+++ src/components/sections/skills/Skills.tsx
@@ -0,0 +1,13 @@
+import Section from '@/components/layout/section/Section';
+import Wrapper from '@/components/layout/wrapper/Wrapper';
+import Bubble from '@/components/sections/skills/Bubble';
+
+export default function Skills() {
+  return (
+    <Section sectionId="skills">
+      <Wrapper>
+        <Bubble />
+      </Wrapper>
+    </Section>
+  );
+}
---
+++ src/components/sections/skills/bubbleColor.ts
@@ -0,0 +1,37 @@
+export const bubbleColor = [
+  {
+    id: 'javascript',
+    text: 'JavaScript',
+    name: 'orange',
+    mainColor: '#fea743',
+    subColor: '#ffa238',
+  },
+  {
+    id: 'css',
+    text: 'CSS',
+    name: 'blue',
+    mainColor: '#1a4cb3',
+    subColor: '#0f42af',
+  },
+  {
+    id: 'react',
+    text: 'React',
+    name: 'purple',
+    mainColor: '#0169b4',
+    subColor: '#005c9e',
+  },
+  {
+    id: 'nextjs',
+    text: 'NextJs',
+    name: 'yellow',
+    mainColor: '#feda10',
+    subColor: '#ffd900',
+  },
+  {
+    id: 'reactquery',
+    text: 'React Query',
+    name: 'pink',
+    mainColor: '#fe2dc0',
+    subColor: '#ff22bd',
+  },
+];
