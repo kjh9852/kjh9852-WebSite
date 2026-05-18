21ba8ced24bc92e087b1937207eac85c90759c97
JeongHyeonKim <kjh985264@gmail.com>
2026-05-18 21:12:46 +0900

    refactor: 배경 물방울 애니메이션 타이밍 미세 조정

--- src/animations/oceanAnimation.ts
+++ src/animations/oceanAnimation.ts
@@ -12,10 +12,10 @@ export default function oceanAnimation(targets: {
   };
 
   const anim1 = gsap.to(targets.bubble, { ...baseAnimation, duration: 30 });
-  const anim2 = gsap.to(targets.midBubble, { ...baseAnimation, duration: 30 });
+  const anim2 = gsap.to(targets.midBubble, { ...baseAnimation, duration: 45 });
   const anim3 = gsap.to(targets.smallBubble, {
     ...baseAnimation,
-    duration: 30,
+    duration: 60,
   });
 
   return [anim1, anim2, anim3];
